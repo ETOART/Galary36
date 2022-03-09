bbb2fbbab293bf4b025afdbed7797c818e3864cb
Rachelforce <55845125+Rachelforce@users.noreply.github.com>
2022-03-09 14:14:35 +0300

    Painting Class Created

---
+++ Assets/Galary36/ARScence/Painting/Scripts/Painting.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : MonoBehaviour
+{
+    [SerializeField] private Texture _texture;
+    [SerializeField] private GameObject _renderPlane;
+
+    private void Awake()
+    {
+        
+    }
+    private void Update()
+    {
+        changePlaneTexture(_texture);
+    }
+
+    private void changePlaneTexture(Texture texture)
+    {
+        ChangePlaneScale(texture.width,texture.height);
+        _renderPlane.GetComponent<Renderer>().material.mainTexture = texture;
+    }
+
+    private void ChangePlaneScale(float width, float height)
+    {
+        _renderPlane.gameObject.transform.localScale = new Vector3(width / height, 1, 1);
+    }
+}
