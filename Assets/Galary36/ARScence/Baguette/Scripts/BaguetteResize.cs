5d049af75fe941543103b5cf0040075a0a3c072f
JanClopVandal <81968795+JanClopVandal@users.noreply.github.com>
2022-03-11 16:09:55 +0300

    Static objects
    Сделал все объекты статичными, добавил материал на бортики и поправил материал на цветной комнате

--- Assets/Galary36/ARScence/Baguette/Scripts/BaguetteResize.cs
+++ Assets/Galary36/ARScence/Baguette/Scripts/BaguetteResize.cs
@@ -4,43 +4,39 @@ using UnityEngine;
 
 public class BaguetteResize : MonoBehaviour
 {
-    [SerializeField] private GameObject paint;
-    private List<GameObject> baguetteObjects; 
-    private Vector3 paintScale;
+    private Transform paint;
+    private List<Transform> baguetteObjects; 
+    private List<Vector3> baguetteScales;
+    private List<Vector3> baguettePosition;
+    
     private  void Start()
     {
-        baguetteObjects = new List<GameObject>();
-        for (int i= 0; i< transform.childCount; i++){
-            baguetteObjects.Add(transform.GetChild(i).gameObject);
-        }
-        ReSizeBaguette(paint.transform.localScale);
+        Initialize();
+        ReSizeBaguette(paint.localScale);
     }
     private void ReSizeBaguette(Vector3 paintScale){
         float xScale = paintScale.x;
         float yScale = paintScale.y;
-
         if(xScale != 1){
-            Vector3 oldScale = baguetteObjects[2].transform.localScale;
-            baguetteObjects[2].transform.localScale = new Vector3(oldScale.x, xScale+xScale*0.1f,oldScale.z);
-            oldScale = baguetteObjects[3].transform.localScale;
-            baguetteObjects[3].transform.localScale = new Vector3(oldScale.x, xScale+xScale*0.1f,oldScale.z);
+            baguetteObjects[2].localScale = new Vector3((baguetteScales[2].x+(xScale-1)),(baguetteScales[2].y),baguetteScales[2].z );
+            baguetteObjects[3].localScale = new Vector3((baguetteScales[3].x+(xScale-1)),(baguetteScales[3].y),baguetteScales[3].z );
 
-            Vector3 oldPosition = baguetteObjects[0].transform.localPosition;
-            baguetteObjects[0].transform.localPosition = new Vector3(oldPosition.x + xScale, oldPosition.y, oldPosition.z);
-            oldPosition = baguetteObjects[1].transform.localPosition;
-            baguetteObjects[1].transform.localPosition = new Vector3(oldPosition.x - xScale, oldPosition.y, oldPosition.z);
-
+            baguetteObjects[2].localPosition = new Vector3((baguettePosition[2].x),(baguettePosition[2].y+(xScale-1)/11),baguettePosition[2].z );
+            baguetteObjects[3].localPosition = new Vector3((baguettePosition[3].x),(baguettePosition[3].y-(xScale-1)/11),baguettePosition[3].z );
         }
-        if(yScale != 1){
-            Vector3 oldScale = baguetteObjects[0].transform.localScale;
-            baguetteObjects[0].transform.localScale = new Vector3(oldScale.x, yScale,oldScale.z);
-            oldScale = baguetteObjects[1].transform.localScale;
-            baguetteObjects[1].transform.localScale = new Vector3(oldScale.x, yScale,oldScale.z);
+     
+        
+    }
+    private void Initialize(){
+        paint = transform.parent.gameObject.GetComponent<Transform>();
 
-            Vector3 oldPosition = baguetteObjects[2].transform.localPosition;
-            baguetteObjects[2].transform.localPosition = new Vector3(oldPosition.x + yScale, oldPosition.y, oldPosition.z);
-            oldPosition = baguetteObjects[3].transform.localPosition;
-            baguetteObjects[3].transform.localPosition = new Vector3(oldPosition.x - yScale, oldPosition.y, oldPosition.z);
+        baguetteObjects = new List<Transform>();
+        baguetteScales = new List<Vector3>();
+        baguettePosition = new List<Vector3>();
+        for (int i= 0; i< transform.childCount; i++){
+            baguetteObjects.Add(transform.GetChild(i).gameObject.transform);
+            baguetteScales.Add(transform.GetChild(i).gameObject.transform.localScale);
+            baguettePosition.Add(transform.GetChild(i).gameObject.transform.localPosition);
         }
     }
 }
